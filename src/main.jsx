b95789579c75c2383511de29feb58b85d5a369ad
E Hemanathan <223499444+hemanathan115@users.noreply.github.com>
2026-03-13 19:15:30 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import HashTool from './HashTool.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashTool />
+  </React.StrictMode>
+)
